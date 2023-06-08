9d2355a7cb07248d3481c0bb824e2bd696f2a1df
LKH <kangholee8512@gmail.com>
2023-06-09 00:21:20 +0900

    환경셋팅

--- app/src/main/java/com/cnc/myapplication/MainActivity.kt
+++ app/src/main/java/com/cnc/myapplication/MainActivity.kt
@@ -2,10 +2,20 @@ package com.cnc.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cnc.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var mBinding: ActivityMainBinding? = null
+    // 매번 null 체크를 할 필요 없이 편의성을 위해 바인딩 변수 재 선언
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+
+
     }
 }
